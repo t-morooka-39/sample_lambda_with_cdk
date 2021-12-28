1f1d510ed4edbb2979e0af5bd1dbe8f1547ac7f2
t.morooka <47558117+t-morooka-39@users.noreply.github.com>
2021-12-28 10:39:29 +0900

    feat: Do "cdk init app --language=typescript" (#1)
    Co-authored-by: t-morooka-39 <morookatakuto@morookatakutonoMacBookPro.local>

---
+++ test/lambda_with_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaWithCdk from '../lib/lambda_with_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda_with_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaWithCdk.LambdaWithCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
