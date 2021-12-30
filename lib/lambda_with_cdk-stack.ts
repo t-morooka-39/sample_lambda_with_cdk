39c131a2a7111ef17a6640763bdb4ed68622adeb
t.morooka <47558117+t-morooka-39@users.noreply.github.com>
2021-12-30 14:53:14 +0900

    Add Lambda and IAM Role in stack (#3)
    * feat: Update typescript version
    
    * feat: Rename lambda/sample/index.ts to lambda/get/index.ts
    
    * feat: Add lambda and iam to stack
    
    Co-authored-by: t-morooka-39 <morookatakuto@morookatakutonoMacBookPro.local>

--- lib/lambda_with_cdk-stack.ts
+++ lib/lambda_with_cdk-stack.ts
@@ -1,16 +1,40 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { aws_iam as iam } from "aws-cdk-lib";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+
+export interface CustomizedProps extends StackProps {
+  projectName: string;
+}
 
 export class LambdaWithCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: CustomizedProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // iam role
+    const iamRoleForLambda = new iam.Role(this, "iamRoleForLambda", {
+      roleName: `${props.projectName}-lambda-role`,
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+      // VPCに設置する場合下記が必要
+      // managedPolicies: [
+      //   iam.ManagedPolicy.fromAwsManagedPolicyName(
+      //     "service-role/AWSLambdaVPCAccessExecutionRole"
+      //   ),
+      // ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaWithCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // lambda
+    const sampleLambda = new NodejsFunction(this, "GETsample", {
+      entry: "lambda/get/index.ts", // どのコードを使用するか
+      runtime: Runtime.NODEJS_14_X, // どのバージョンか
+      timeout: Duration.seconds(30), // 何秒でタイムアウトするか
+      role: iamRoleForLambda, // どのIAMロールを使用するか
+      // vpc: vpc,
+      environment: {
+        AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1", // keepaliveを有効にする
+      },
+      memorySize: 128, // default=128
+    });
   }
 }
